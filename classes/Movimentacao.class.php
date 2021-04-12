0b00b5b288a551c2235ca4c3647251bcb971e924
TamirisR <Tamis_rodrigues@hotmail.com>
2021-04-12 01:32:27 -0300

    exercicio de poo em aula

---
+++ classes/Movimentacao.class.php
@@ -0,0 +1,15 @@
+<?php
+
+require(__DIR__ . '/../interfaces/item.interface.php');
+
+class Movimentacao implements iMovimentacao
+{
+
+    public function setDados(array $dados): bool
+    {
+    }
+
+    public function getDados(string $tipo, string $datahora, int $id_usuario): array
+    {
+    }
+}
