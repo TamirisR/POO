2e2b31c73f3e7f9e09771742addb23052756ef06
TamirisR <Tamis_rodrigues@hotmail.com>
2021-04-20 19:36:31 -0300

    praticando POO

--- classes/abstratas/TIpoPessoa.class.php
+++ classes/abstratas/TIpoPessoa.class.php
@@ -5,4 +5,9 @@ abstract class TipoPessoa
 
     protected $id;
     protected $nome;
+    public function __construct(){
+
+		parent::__construct();
+	}
+}
 }
--- estoque.php
+++ estoque.php
@@ -17,13 +17,72 @@ class Main
         $objFabricante = new Fabricante;
         $objEstoque = new Estoque;
         $objMovimentacao = new Movimentacao;
+    
+        switch ($_SERVER['argv'][1]) {  
+
+        case 'gravaUsuario':
+
+            $this->gravaUsuario($objUsuario);
+            break;
+
+        case 'editaUsuario':
+
+            $this->editaUsuario($objUsuario);
+            break;				
+        
+        default:
+            
+            echo "\nERRO: Não existe a funcionalidade {$_SERVER['argv'][1]}\n";				
+    }
+    
+}
+
+public function editaUsuario($objUsuario){
+
+    $dados = $this->tratarDados();
+
+    $objUsuario->setDados($dados);
+
+    if( $objUsuario->gravarDados() ){
+
+        echo "\nUsuário editado com sucesso!\n";
     }
 
-    public function __destruct()
-    {
+}
 
-        echo "\n\n--- FIM DO PROGRAMA ---\n\n";
+public function gravaUsuario($objUsuario){
+
+    $dados = $this->tratarDados();
+
+    $objUsuario->setDados($dados);
+
+    if( $objUsuario->gravarDados() ){
+
+        echo "\nUsuário gravado com sucesso!\n";
     }
+
+}
+
+public function tratarDados(){
+
+    $args = explode( ',', $_SERVER['argv'][2]); 
+
+    foreach ($args as $valor) {
+        
+        $arg = explode('=', $valor);
+
+        $dados[$arg[0]] = $arg[1];
+    }
+
+    return $dados;
+}
+
+public function __destruct(){
+
+    sleep(1);
+
+    echo "\n\n--- FIM DO PROGRAMA ---\n\n";
+}
 }
 
 new Main;
